3117dcc0aa490076b495b35ce2e55c103a6360dd
Simon <rsemenistov@gmail.com>
2025-03-12 18:31:05 +0300

    add filter by state function

---
+++ src/processing.py
@@ -0,0 +1,9 @@
+def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    """
+    Функция фильтрует список словарей по значению ключа 'state'.
+
+    :param data: Список словарей для фильтрации
+    :param state: Значение ключа 'state' (по умолчанию 'EXECUTED')
+    :return: Новый список словарей, соответствующих указанному значению state
+    """
+    return [item for item in data if item.get('state') == state]
