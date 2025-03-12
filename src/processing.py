df1fbab3f6f1108dca2be0fa7b24631d45a32297
Simon <rsemenistov@gmail.com>
2025-03-12 18:44:29 +0300

    add function for sorting

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,13 @@ def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
     :return: Новый список словарей, соответствующих указанному значению state
     """
     return [item for item in data if item.get('state') == state]
+
+def sort_by_date(data: list[dict], descending: bool = True) -> list[dict]:
+    """
+    Функция сортирует список словарей по значению ключа 'date'.
+
+    :param data: Список словарей для фильтрации
+    :param descending: Направление сортировки
+    :return: Новый отсортированный список словарей
+    """
+    return sorted(data, key=lambda x: x.get("date"), reverse=descending)
