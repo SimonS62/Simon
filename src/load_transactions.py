24a2433a7aac2424da7f41ea319f7a12d1c09047
Simon <rsemenistov@gmail.com>
2025-06-14 17:53:29 +0300

    homework_10.7

--- src/load_transactions.py
+++ src/load_transactions.py
@@ -8,7 +8,7 @@ def read_transactions_csv(file_path):
     :return: список словарей с транзакциями
     """
     try:
-        df = pd.read_csv(file_path)
+        df = pd.read_csv(file_path, sep=';')
         # Преобразуем DataFrame в список словарей
         transactions = df.to_dict(orient='records')
         return transactions
@@ -25,7 +25,6 @@ def read_transactions_xlsx(file_path):
     """
     try:
         df = pd.read_excel(file_path)
-        df = pd.read_csv(file_path, sep=';')
         # Преобразуем DataFrame в список словарей
         transactions = df.to_dict(orient='records')
         return transactions
